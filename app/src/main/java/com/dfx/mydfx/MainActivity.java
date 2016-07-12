60c206bc44061772a101c73df475e495be49104c
tbwdxd <taylortian@csu.fullerton.edu>
2016-07-12 14:09:17 -0700

    Dropped fragment method, use spannable string instead

--- app/src/main/java/com/dfx/mydfx/MainActivity.java
+++ app/src/main/java/com/dfx/mydfx/MainActivity.java
@@ -1,6 +1,5 @@
 package com.dfx.mydfx;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -9,8 +8,11 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableStringBuilder;
+import android.text.style.ImageSpan;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -20,11 +22,12 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Set Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-
+        //Set Drawer and its functions
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.setScrimColor(Color.TRANSPARENT);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -32,9 +35,13 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-
+        //enable item selected listener
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //set the default selected tab to the first tab
+        navigationView.setCheckedItem(R.id.nav_profile);
+        navigationView.getMenu().performIdentifierAction(R.id.nav_profile, 0);
     }
 
     @Override
@@ -77,10 +84,37 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_profile) {
             // Handle the camera action
+
+            //this is the fragment method
+            /*MainFragment fragment = new MainFragment();
+            android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame,fragment);
+            fragmentTransaction.commit();*/
+
+            //instead of using fragments, we use spannable String to change views.
+            SpannableStringBuilder builder = new SpannableStringBuilder();
+            builder.append("My string. I ").append(" ");
+            builder.setSpan(new ImageSpan(this, R.drawable.ic_menu_camera),
+                    builder.length() - 1, builder.length(), 0);
+            builder.append(" Cree by Dexode");
+            TextView textView = (TextView) findViewById(R.id.maintext);
+            textView.setText(builder);
+
+
         } else if (id == R.id.nav_manual){
-        Intent intent = new Intent(MainActivity.this, ManualActivity.class);
-        startActivity(intent);
-        return true;
+            SpannableStringBuilder builder = new SpannableStringBuilder();
+            builder.append("My string. You ").append(" ");
+            builder.setSpan(new ImageSpan(this, R.drawable.ic_menu_gallery),
+                    builder.length() - 1, builder.length(), 0);
+            builder.append(" Cree by Dexode");
+            TextView textView = (TextView) findViewById(R.id.maintext);
+            textView.setText(builder);
+
+            //this is the fragment method
+            /*ContentFragment fragment = new ContentFragment();
+            android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame,fragment);
+            fragmentTransaction.commit();*/
         } /*else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
